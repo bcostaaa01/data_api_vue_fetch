6445a78301ba08272f58c8fe0fe1e22a8389e990
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-10-27 13:35:16 +0100

    First commit: add source code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<TodosComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodosComponent from "./components/TodosComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodosComponent
   }
 }
 </script>
---
+++ src/components/TodosComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ heading }}</h1>
+    <div v-for="(item, index) in todos" :key="index">
+      <ul>
+        <section>
+          <h4>{{ item.userId }}</h4>
+          <h4>{{ item.title }}</h4>
+        </section>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodosComponent",
+  data() {
+    return {
+      heading: "Todos from API",
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => response.json())
+      .then((json) => (this.todos = json));
+  },
+};
+</script>
